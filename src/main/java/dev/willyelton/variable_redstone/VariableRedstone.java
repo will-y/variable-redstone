6765a24d4d644ace038eaa32ba847843c3037c01
Will Yelton <willhyelton@gmail.com>
2025-11-08 12:43:57 -0500

    Removes example mod stuff

---
+++ src/main/java/dev/willyelton/variable_redstone/VariableRedstone.java
@@ -0,0 +1,20 @@
+package dev.willyelton.variable_redstone;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(VariableRedstone.MODID)
+public class VariableRedstone {
+    public static final String MODID = "variable_redstone";
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public VariableRedstone(IEventBus modEventBus, ModContainer modContainer) {
+    }
+}
